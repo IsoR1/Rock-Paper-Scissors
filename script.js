f85139c55a74ff85663e6ec25f2b197fd53e9e82
Braco <isovicramiz07@gmail.com>
2022-09-24 02:33:57 -0400

    added gui

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+let userScore = 0;
+let computerScore = 0;
+
+rock.addEventListener("click", (e) => {
+    playRound(e.target.innerText, getComputerChoice())
+})
+
+paper.addEventListener("click", (e) => {
+    playRound(e.target.innerText, getComputerChoice())
+})
+
+scissors.addEventListener("click", (e) => {
+    playRound(e.target.innerText, getComputerChoice())
+})
+
+function renderResults() {
+    const uResult = document.querySelector(".user-score");
+    uResult.innerHTML = `The User Score is: ${userScore}`;
+
+    const computerResult = document.querySelector(".computer-score");
+    computerResult.innerHTML = `The Computer Score is: ${computerScore}`;
+}
+
+renderResults();
+
 function getComputerChoice() {
     const options = ['Rock', 'Paper', 'Scissors'];
     const choice = Math.ceil(Math.random() * options.length);
@@ -24,18 +52,39 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'scissors' ||
         playerSelection == 'scissors' && computerSelection == 'paper' ||
         playerSelection == 'paper' && computerSelection == 'rock') {
-            return 'Player wins!'
+            winnerDisplay(' Player wins! ');
+            userScore++;
+            renderResults();
+            if (userScore == 5) {
+                clearDisplay();
+                winnerDisplay(" Player wins the game! ");
+                resetScore();
+            }
         } else if (playerSelection == computerSelection) {
-            return 'Draw!';
+            winnerDisplay(' Draw ');
         } else {
-            return 'Computer wins!';
-    }
- }
-
-function game() {
-    for (let i = 0; i < 5; i++) { 
-        console.log(playRound(playerChoice(), getComputerChoice()));
+            winnerDisplay(" Computer Wins! ");
+            computerScore++;
+            renderResults();
+            if (computerScore == 5) {
+                clearDisplay();
+                winnerDisplay(" Computer Wins the game! ");
+                resetScore();
+            }
     }
 }
 
-game()
+function winnerDisplay(msg) {
+    const results = document.querySelector('.results-display');
+    results.append(msg)
+}
+
+function clearDisplay() {
+    const results = document.querySelector('.results-display');
+    results.innerHTML = ''
+}
+
+function resetScore() {
+    userScore = 0;
+    computerScore = 0;
+}
